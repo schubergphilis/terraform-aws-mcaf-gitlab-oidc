ce60af1cc252f96f75cbfe906c9466e706cbbc6a
Marwin Baumann <mbaumann@schubergphilis.com>
2025-02-28 10:40:02 +0100

    breaking: ensure module works for all oidc setups

--- main.tf
+++ main.tf
@@ -1,28 +1,40 @@
 locals {
-  provider_arn = var.create_provider ? aws_iam_openid_connect_provider.gitlab["instance"].arn : data.aws_iam_openid_connect_provider.gitlab["instance"].arn
-  provider_url = var.create_provider ? aws_iam_openid_connect_provider.gitlab["instance"].url : data.aws_iam_openid_connect_provider.gitlab["instance"].url
+  provider = coalesce(var.github_provider, var.gitlab_provider, var.custom_provider)
+
+  provider_arn = var.create_provider ? aws_iam_openid_connect_provider.default["instance"].arn : data.aws_iam_openid_connect_provider.default["instance"].arn
+  provider_url = var.create_provider ? aws_iam_openid_connect_provider.default["instance"].url : data.aws_iam_openid_connect_provider.default["instance"].url
 }
+
+
+################################################################################
+# OIDC Provider
+################################################################################
 
 # We avoid using https scheme because the Hashicorp TLS provider has started following redirects starting v4.
 # See https://github.com/hashicorp/terraform-provider-tls/issues/249
-data "tls_certificate" "gitlab" {
-  url = "${replace(var.gitlab_url, "https", "tls")}:443"
+data "tls_certificate" "fingerprint" {
+  url = "${replace(local.provider.url, "https", "tls")}:443"
 }
 
-data "aws_iam_openid_connect_provider" "gitlab" {
+data "aws_iam_openid_connect_provider" "default" {
   for_each = !var.create_provider ? { instance = true } : {}
 
-  url = var.gitlab_url
+  url = local.provider.url
 }
 
-resource "aws_iam_openid_connect_provider" "gitlab" {
+resource "aws_iam_openid_connect_provider" "default" {
   for_each = var.create_provider ? { instance = true } : {}
 
-  url             = var.gitlab_url
-  client_id_list  = [var.gitlab_url]
-  thumbprint_list = [data.tls_certificate.gitlab.certificates[0].sha1_fingerprint]
+  url             = local.provider.url
+  client_id_list  = local.provider.client_ids
+  thumbprint_list = [data.tls_certificate.fingerprint.certificates[0].sha1_fingerprint]
   tags            = var.tags
 }
+
+################################################################################
+# AWS IAM Role
+################################################################################
+
 
 data "aws_iam_policy_document" "assume_role_policy" {
   for_each = var.iam_roles
@@ -36,13 +48,24 @@ data "aws_iam_policy_document" "assume_role_policy" {
       identifiers = [local.provider_arn]
     }
 
+    # GitLab
     # A concatenation of metadata describing the GitLab CI/CD workflow including the group, project, branch, and tag. The sub field is in the following format:
     # project_path:{group}/{project}:ref_type:{type}:ref:{branch_name}
     # https://docs.gitlab.com/ee/ci/cloud_services/index.html#configure-a-conditional-role-with-oidc-claims
+
+    # GitHub
+    # todo
+    # https://docs.github.com/en/actions/security-for-github-actions/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services
     condition {
       test     = "StringLike"
       variable = "${local.provider_url}:sub"
-      values   = ["project_path:${each.value.subject_filter_allowed.path}:ref_type:${each.value.subject_filter_allowed.ref_type}:ref:${each.value.subject_filter_allowed.ref}"]
+      values = compact([
+        each.value.subject_filter_allowed_gitlab != null ? "project_path:${each.value.subject_filter_allowed_gitlab.project_path}:ref_type:${each.value.subject_filter_allowed_gitlab.ref_type}:ref:${each.value.subject_filter_allowed_gitlab.ref}" : null,
+        each.value.subject_filter_allowed_github != null ? "repo:${each.value.subject_filter_allowed_github.repository}:ref:refs/heads/${each.value.subject_filter_allowed_github.branch}" : null,
+        each.value.subject_filter_allowed_github != null && each.value.subject_filter_allowed_github.environment != null ? "repo:${each.value.subject_filter_allowed_github.repository}:env:${each.value.subject_filter_allowed_github.environment}" : null,
+        each.value.subject_filter_allowed_github != null && each.value.subject_filter_allowed_github.tag != null ? "repo:${each.value.subject_filter_allowed_github.repository}:ref:refs/tags/${each.value.subject_filter_allowed_github.tag}" : null,
+        each.value.subject_filter_allowed_custom
+      ])
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -4,38 +4,124 @@ variable "create_provider" {
   description = "Toggle to whether or not create the provider. Put to false to not create the provider but instead data source it and create roles only."
 }
 
-variable "gitlab_url" {
-  type        = string
-  default     = "https://gitlab.com"
-  description = "GitLab URL. The address of your GitLab instance, such as https://gitlab.com or https://gitlab.example.com."
+variable "custom_provider" {
+  type = object({
+    enabled     = optional(bool, false)
+    name        = string
+    url         = string
+    fingerprint = string
+    client_ids  = list(string)
+  })
+  default     = null
+  description = "Create a custom provider."
 
   validation {
-    condition     = can(regex("^https", var.gitlab_url))
-    error_message = "URL should start with 'https'."
+    condition = (
+      (var.github_provider != null && var.github_provider.enabled ? 1 : 0) +
+      (var.gitlab_provider != null && var.gitlab_provider.enabled ? 1 : 0) +
+      (var.custom_provider != null && var.custom_provider.enabled ? 1 : 0)
+    ) == 1
+    error_message = "Exactly one provider must be enabled. Please enable only one of github_provider, gitlab_provider, or custom_provider."
   }
+}
+
+variable "github_provider" {
+  type = object({
+    enabled     = optional(bool, false)
+    name        = optional(string, "GitHub")
+    url         = optional(string, "https://token.actions.githubusercontent.com")
+    fingerprint = optional(string, "https://token.actions.githubusercontent.com/.well-known/openid-configuration")
+    client_ids  = list(string, ["sts.amazonaws.com"])
+  })
+  default     = null
+  description = "Create a github provider."
+}
+
+variable "gitlab_provider" {
+  type = object({
+    enabled     = optional(bool, false)
+    name        = optional(string, "GitLab")
+    url         = optional(string, "https://gitlab.com")
+    fingerprint = optional(string, "https://gitlab.com")
+    client_ids  = list(string, ["https://gitlab.com"])
+  })
+  default     = null
+  description = "Create a github provider."
 }
 
 variable "iam_roles" {
   type = map(object({
-    description              = optional(string, "Role assumed by the Gitlab IAM OIDC provider")
+    description              = optional(string, "Role assumed by the IAM OIDC provider")
     name                     = optional(string, null)
     path                     = optional(string, "/")
     permissions_boundary_arn = optional(string, "")
     policy                   = optional(string, null)
     policy_arns              = optional(set(string), [])
 
-    subject_filter_allowed = object({
-      path     = string
-      ref_type = string
-      ref      = string
-    })
+    subject_filter_allowed_custom = optional(string)
+
+    subject_filter_allowed_gitlab = optional(object({
+      project_path = string
+      ref_type     = string
+      ref          = string
+    }))
+
+    subject_filter_allowed_github = optional(object({
+      repository  = string
+      branch      = optional(string)
+      environment = optional(string)
+      tag         = optional(string)
+    }))
   }))
+
   default     = {}
   description = "Configuration for IAM roles, the key of the map is used as the IAM role name. Unless overwritten by setting the name field."
 
   validation {
-    condition     = alltrue([for o in var.iam_roles : can(regex("^(\\*|branch|tag)$", o.subject_filter_allowed.ref_type))])
-    error_message = "ref_type must be '*', 'branch', or 'tag'."
+    condition = alltrue([
+      for role in values(var.iam_roles) : (
+        length(compact([
+          role.subject_filter_allowed_custom,
+          role.subject_filter_allowed_gitlab,
+          role.subject_filter_allowed_github
+        ])) == 1
+      )
+    ])
+    error_message = "Each IAM role must have exactly one subject filter defined (choose one of subject_filter_allowed_custom, subject_filter_allowed_gitlab, or subject_filter_allowed_github)."
+  }
+
+  validation {
+    condition = alltrue([
+      for role in values(var.iam_roles) : (
+        role.subject_filter_allowed_gitlab == null ||
+        can(regex("^(\\*|branch|tag)$", role.subject_filter_allowed_gitlab.ref_type))
+      )
+    ])
+    error_message = "If subject_filter_allowed_gitlab is define, ref_type must be '*', 'branch', or 'tag'."
+  }
+
+  validation {
+    condition = alltrue([
+      for role in values(var.iam_roles) : (
+        role.subject_filter_allowed_github == null ||
+        length(compact([
+          role.subject_filter_allowed_github.branch != null ? role.subject_filter_allowed_github.branch : null,
+          role.subject_filter_allowed_github.environment != null ? role.subject_filter_allowed_github.environment : null,
+          role.subject_filter_allowed_github.tag != null ? role.subject_filter_allowed_github.tag : null
+        ])) == 1
+      )
+    ])
+    error_message = "If subject_filter_allowed_github is defined, exactly one of branch, environment, or tag must be specified."
+  }
+
+  validation {
+    condition = alltrue([
+      for role in values(var.iam_roles) : (
+        role.subject_filter_allowed_github == null ||
+        length(regexall("^[A-Za-z0-9_.-]+?/([A-Za-z0-9_.:/\\-\\*]+)$", role.subject_filter_allowed_github.repository)) > 0
+      )
+    ])
+    error_message = "If subject_filter_allowed_github is defined, the repository must be in the organization/repository format."
   }
 }
 
